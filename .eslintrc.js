650c6325091ea6ed3f77d6e29800471447609035
Antti Sartjärvi <antti.sartjarvi@gmail.com>
2018-12-16 02:43:32 +0200

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'plugins': [
+    'import',
+    'react',
+  ],
+  'rules': {
+    'react/jsx-uses-vars': 2,
+    'react/jsx-indent': [2, 2],
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+  },
+};
